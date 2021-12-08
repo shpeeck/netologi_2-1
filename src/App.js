0ddf824b3eb74314ad612732941aeb4d91e4e23b
shpeeck <67144527+shpeeck@users.noreply.github.com>
2021-12-08 19:57:11 +0200

    dz-2-1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./css/App.css";
+import Portfolio from "./components/Portfolio/Portfolio";
+// import Toolbar from "./components/Toolbar/Toolbar";
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
